d421468ca062e381ac4403bcb38afcd172244761
Prthvi <prathviachar02@gmail.com>
2025-11-14 15:50:35 +0530

    Updated food app project

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -7,7 +7,7 @@ class FirestoreService {
 
   // Stream to listen for real-time updates of the food list
   Stream<List<FoodItem>> getFoodItems() {
-    return _db.collection('food_items').snapshots().map((snapshot) {
+    return _db.collection('food_item').snapshots().map((snapshot) {
       return snapshot.docs.map((doc) {
         return FoodItem.fromFirestore(doc.data(), doc.id);
       }).toList();
